002843d616c48c4d5ba02896fcb69c9b9248c782
jorge <jae13@outlook.es>
2019-11-09 22:51:10 +0100

    tema1: estructura clases y comentarios

---
+++ src/basic/ClassesVSFiles.java
@@ -0,0 +1,5 @@
+package basic;
+
+public class ClassesVSFiles {
+    String name;
+}
---
+++ src/basic/Comments.java
@@ -0,0 +1,31 @@
+package basic;
+
+public class Comments {
+    //comment until end of line
+
+    /*Multiple
+      line comment
+     */
+
+
+    /**
+     * Javadoc multiple-line comment
+     * @author Jorge Aznar Escolano
+     */
+
+    //ejemplos
+
+    /*
+     * //anteater multiple
+     */
+    //bear  single
+    // // cat single
+
+    // /* dog */ single
+
+    /*
+    * /* ferret */ // dont compile
+    */
+
+
+}
